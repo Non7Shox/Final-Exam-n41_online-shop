c52158dcf34425e100cbf234771d06e2fa1e4723
Non7Shox <muhammadsodiqkasimov34@gmail.com>
2024-07-16 16:59:24 +0500

    first commit

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.urls', namespace='users')),
     path('api/v1/products/', include('products.urls', namespace='products')),
+    path('api/v1/cart/', include('cartandorders.urls', namespace='cartandorders')),
 ]
